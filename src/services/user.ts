c6af37885746b6e5a3e2a2abed4b24ea63668d81
harshpx <harsh.rzf@gmail.com>
2025-07-29 12:27:34 +0530

    added users features

---
+++ src/services/user.ts
@@ -0,0 +1,38 @@
+import { baseUrl } from "@/lib/utils";
+
+export const registerUser = async (
+  username: string,
+  email: string,
+  password: string,
+) => {
+  let url = `${baseUrl}/api/v1/users/register`;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  };
+  const data = await fetch(url, options);
+  return await data.json();
+};
+
+export const loginUser = async (identifier: string, password: string) => {
+  let url = `${baseUrl}/api/v1/users/login`;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      identifier,
+      password,
+    }),
+  };
+  const data = await fetch(url, options);
+  return await data.json();
+};
